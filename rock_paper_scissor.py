10385e854111689311f739afcffce7ebb3003b68
Sadnan Islam <sadnan15916q@gmail.com>
2024-12-06 15:21:19 +0600

    add rounds system

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -7,7 +7,7 @@ def pick_choice(user_input):
         return 'paper'
     if user_input == 's' or user_input == 'scissors':
         return 'scissor'
-    
+
     return None
 
 def print_symbols(symbol):
@@ -42,38 +42,109 @@ def print_symbols(symbol):
 ---.__________)
 ''')
 
-game_outcomes = ['rock', 'paper', 'scissor']
-user_choice = input("'Rock', 'Paper', 'Scissor' - INPUT! (r/p/s)\n").lower()
-bot_choice = random.choice(game_outcomes)
+def ending_print(uc,bc):
+    print("""
+██████████████████████████████████████████████████████████████████
+█▄─▄▄─█▄─▀█▄─▄█▄─▄▄▀███─▄▄─█▄─▄▄─███▄─▀█▀─▄██▀▄─██─▄─▄─█─▄▄▄─█─█─█
+██─▄█▀██─█▄▀─███─██─███─██─██─▄██████─█▄█─███─▀─████─███─███▀█─▄─█
+▀▄▄▄▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▀▀▀▀▄▄▄▄▀▄▄▄▀▀▀▀▀▄▄▄▀▄▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▀▄▀""")
 
+    if uc>bc:
+        return """
+██╗░░░██╗░█████╗░██╗░░░██╗  ░██╗░░░░░░░██╗██╗███╗░░██╗
+╚██╗░██╔╝██╔══██╗██║░░░██║  ░██║░░██╗░░██║██║████╗░██║
+░╚████╔╝░██║░░██║██║░░░██║  ░╚██╗████╗██╔╝██║██╔██╗██║
+░░╚██╔╝░░██║░░██║██║░░░██║  ░░████╔═████║░██║██║╚████║
+░░░██║░░░╚█████╔╝╚██████╔╝  ░░╚██╔╝░╚██╔╝░██║██║░╚███║
+░░░╚═╝░░░░╚════╝░░╚═════╝░  ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝"""
 
-if user_choice not in game_outcomes:
-    user_choice = pick_choice(user_choice)
-    if user_choice is None:
-        print("You got confused, you loose.")
+    elif bc>uc:
+        return """
+██╗░░░██╗░█████╗░██╗░░░██╗  ██╗░░░░░░█████╗░░██████╗███████╗
+╚██╗░██╔╝██╔══██╗██║░░░██║  ██║░░░░░██╔══██╗██╔════╝██╔════╝
+░╚████╔╝░██║░░██║██║░░░██║  ██║░░░░░██║░░██║╚█████╗░█████╗░░
+░░╚██╔╝░░██║░░██║██║░░░██║  ██║░░░░░██║░░██║░╚═══██╗██╔══╝░░
+░░░██║░░░╚█████╔╝╚██████╔╝  ███████╗╚█████╔╝██████╔╝███████╗
+░░░╚═╝░░░░╚════╝░░╚═════╝░  ╚══════╝░╚════╝░╚═════╝░╚══════╝"""
 
-print('You chose:')
-print_symbols(user_choice)
-print('Opponent Chose:')
-print_symbols(bot_choice)
-
-if user_choice == bot_choice:
-    print("It's a Draw!")
-
-elif user_choice == 'rock':
-    if bot_choice != 'paper':
-        print('You Win!')
     else:
-        print('You Loose.')
-        
-elif user_choice == 'paper':
-    if bot_choice != 'scissor':
+        return '''
+██╗████████╗██╗░██████╗  ░█████╗░  ██████╗░██████╗░░█████╗░░██╗░░░░░░░██╗
+██║╚══██╔══╝╚█║██╔════╝  ██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║
+██║░░░██║░░░░╚╝╚█████╗░  ███████║  ██║░░██║██████╔╝███████║░╚██╗████╗██╔╝
+██║░░░██║░░░░░░░╚═══██╗  ██╔══██║  ██║░░██║██╔══██╗██╔══██║░░████╔═████║░
+██║░░░██║░░░░░░██████╔╝  ██║░░██║  ██████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░
+╚═╝░░░╚═╝░░░░░░╚═════╝░  ╚═╝░░╚═╝  ╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░'''
+
+def rounds_calc():
+    round_inp = input("How many rounds? [Odds Recommended, default 3]")
+
+    if round_inp == '':
+        return 3
+
+    try:
+        return int(round_inp)
+    except ValueError:
+        print("Please enter a valid positive, integer (no fractional) value or keep blank")
+        return rounds_calc()
+
+    return 3
+
+def rock_paper_scissors():
+    user_choice = input("'Rock', 'Paper', 'Scissor' - INPUT! (r/p/s)\n").lower()
+    game_outcomes = ['rock', 'paper', 'scissor']
+    bot_choice = random.choice(game_outcomes)
+
+
+    if user_choice not in game_outcomes:
+        user_choice = pick_choice(user_choice)
+        if user_choice is None:
+            print("You got confused, Try Again!")
+            rock_paper_scissors()
+
+    print('You chose:')
+    print_symbols(user_choice)
+    print('Opponent Chose:')
+    print_symbols(bot_choice)
+
+    if user_choice == bot_choice:
+        return None
+
+    elif user_choice == 'rock':
+        if bot_choice != 'paper':
+            return True
+        else:
+            return False
+
+    elif user_choice == 'paper':
+        if bot_choice != 'scissor':
+            return True
+        else:
+            return False
+
+    elif user_choice == 'scissor':
+        if bot_choice != 'rock':
+            return True
+        else:
+            return False
+
+rounds = rounds_calc()
+user_score = 0
+bot_score = 0
+print(f'Your Score: {user_score}\nAI Score:{bot_score}')
+for i in range(rounds):
+    print(f'Round {i+1}, Begin!')
+    status = rock_paper_scissors()
+
+    if status is None:
+        print("It's a Draw!")
+    elif status:
+        user_score += 1
         print('You Win!')
-    else:
+    elif not status:
+        bot_score += 1
         print('You Loose.')
-        
-elif user_choice == 'scissor':
-    if bot_choice != 'rock':
-        print('You Win!')
-    else:
-        print('You Loose.')
+
+    print(f'Your Score: {user_score}\nAI Score:{bot_score}')
+
+print(ending_print(user_score, bot_score))
